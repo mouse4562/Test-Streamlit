af5042f72b0502f7c53a7660db5e524d2a6f62fe
unknown <mmshinoda1970@gmail.com>
2023-05-30 14:06:45 +0900

    1st commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import time
+
+#progressbar
+
+st.write("プログレスバーの表示")
+'start!'
+latest_iteration = st.empty()
+bar = st.progress(0)
+
+for i in range(100):
+    latest_iteration.text(f"Iteration {i+1}")
+    bar.progress(i+1)
+    time.sleep(0.01)
+
+st.write("Done!")
